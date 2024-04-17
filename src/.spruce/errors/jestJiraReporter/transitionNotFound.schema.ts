7b10f31eab6db0bf702d99c3e30a4aa44d14de41
Taylor Romero <me@taylorrome.ro>
2024-04-17 14:16:43 -0600

    patch: upgrade

--- src/.spruce/errors/jestJiraReporter/transitionNotFound.schema.ts
+++ src/.spruce/errors/jestJiraReporter/transitionNotFound.schema.ts
@@ -1,22 +1,26 @@
 import { SchemaRegistry } from '@sprucelabs/schema'
 import { SpruceErrors } from '../errors.types'
 
-
-
-const transitionNotFoundSchema: SpruceErrors.JestJiraReporter.TransitionNotFoundSchema  = {
-	id: 'transitionNotFound',
-	namespace: 'JestJiraReporter',
-	name: 'Transition not found',
-	    fields: {
-	            /** Transition. */
-	            'transition': {
-	                label: 'Transition',
-	                type: 'select',
-	                isRequired: true,
-	                options: {choices: [{"value":"In Progress","label":"In Progress"},{"value":"Done","label":"Done"}],}
-	            },
-	    }
-}
+const transitionNotFoundSchema: SpruceErrors.JestJiraReporter.TransitionNotFoundSchema =
+    {
+        id: 'transitionNotFound',
+        namespace: 'JestJiraReporter',
+        name: 'Transition not found',
+        fields: {
+            /** Transition. */
+            transition: {
+                label: 'Transition',
+                type: 'select',
+                isRequired: true,
+                options: {
+                    choices: [
+                        { value: 'In Progress', label: 'In Progress' },
+                        { value: 'Done', label: 'Done' },
+                    ],
+                },
+            },
+        },
+    }
 
 SchemaRegistry.getInstance().trackSchema(transitionNotFoundSchema)
 
